b46cfaa50315b1b67dec809910affe328d11d23a
Caleb Piekstra <piekstra17@up.edu>
2015-09-30 01:22:29 -0700

    Added standard frequencies

--- letter_frequency.py
+++ letter_frequency.py
@@ -1,4 +1,5 @@
 import string
+
 class LetterFrequency:
     
     def getFrequencies(self, msg):
@@ -13,3 +14,11 @@ class LetterFrequency:
             # return print "%c frequency: %0.2f%%" % (letter, frequency)
             
         return frequencies
+    
+    ## getLetterFrequencies
+    #
+    # returns the most common letters found in English sentences
+    # as well as their corresponding frequencies
+    #
+    def getLetterFrequencies(self):
+        return [('e', 12.702),('t', 9.056), ('a', 8.167), ('o', 7.507), ('i', 6.966), ('n', 6.749), ('s', 6.327), ('h', 6.094), ('r', 5.987)]
