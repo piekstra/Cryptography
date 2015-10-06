2a5b841ee9d54b3b77a954fde4effb6309b17708
Caleb Piekstra <piekstra17@up.edu>
2015-10-05 22:15:31 -0700

    Fixed both encryption and decryption
    Added a new feature that multiplies two matrices
    Arguments are now the only way to run the program for
    encryption/decryption

--- hill_encrypt.py
+++ hill_encrypt.py
@@ -1,18 +1,23 @@
+import string, argparse, math
 
 class HillSystem:
 
-    def __init__(self, key):
-        if len(key) < 1:
-            print("ERROR: invalid key")            
-            return
-            
-        # initialize the n x n matrix
-        self.key = key
-        # initialize the key's dimension (n)
-        self.keyDim = len(key[0])
+    def __init__(self):            
+        # initialize a string of chars that cannot be encrypted
+        self.illegalChars = string.punctuation + ' ' 
+        # initialize the character used to pad messages to fit the blocks
+        self.padChar = 'x'
         
     def matrixMult(self, matrix1, matrix2):
-        return
+        multMatrix = []
+        for row in matrix1:
+            multRow = []
+            for col in self.transpose(matrix2):
+                # multiply each element in matrix1's row with the corresponding element in
+                # matrix2's col and sum the results
+                multRow.append(sum([rowEl*colEl for rowEl,colEl in zip(row,col)]))
+            multMatrix.append(multRow)
+        return multMatrix
     
     ## det
     # recursively determines the determinant of an n x n matrix
@@ -42,11 +47,7 @@ class HillSystem:
         adjugateMatrix = self.getAdjugateMatrix(matrix)
         inverseMatrix = [[colEl/matrixDet for colEl in row] for row in adjugateMatrix]
         return inverseMatrix
-        
-    ## inverts the key
-    def invertKey(self):
-        self.key = self.invertMatrix(self.key)
-    
+            
     def getAdjugateMatrix(self, matrix, transpose=True):
         # make sure the matrix is transposed
         if transpose:
@@ -61,37 +62,66 @@ class HillSystem:
             adjugateMatrix.append(detRow)
         return adjugateMatrix        
     
-    def encrypt(self, msg, decrypt=False):
+    def encrypt(self, msg, key, decrypt=False):
+        keyDim = len(key)
+        msg = msg.lower().translate(string.maketrans("", ""), self.illegalChars)
+        # determine if we need to pad 'x's at the end of the msg to make it fill a block
+        numToFill = (keyDim - (len(msg) % keyDim)) % keyDim
+        msg += numToFill * self.padChar
         encryptedMsg = ""
-        for block in [msg[i:i+self.keyDim] for i in range(0,len(msg), self.keyDim)]:
-            for row in self.key:
-                ciphertext = 0
-                for idx, char in enumerate(block):
-                    plaintextInt = ord(char)-96
-                    cipherInt = row[idx]*plaintextInt
-                    ciphertext += cipherInt
-                encryptedMsg += (chr(ciphertext%26+96))                   
+        blocks = [msg[i:i+keyDim] for i in range(0,len(msg),keyDim)]
+        for block in [msg[i:i+keyDim] for i in range(0,len(msg),keyDim)]:
+            encryptedMatrix = self.matrixMult(key, [[ord(char)-96] for char in block])
+            for encryptedRow in encryptedMatrix:
+                cipherInt = encryptedRow[0] % 26
+                # cover the 'z' case
+                if cipherInt == 0:
+                    cipherInt = 26
+                encryptedMsg += chr(cipherInt+96)
         return encryptedMsg if decrypt else encryptedMsg.upper()
     
-    def decrypt(self, msg):
+    def decrypt(self, msg, key):
         # remove spaces from the message
         msg = ''.join(msg.split())
         # invert the key for decryption of the msg
-        self.invertKey()
-        return self.encrypt(msg.lower(), decrypt=True)
+        return self.encrypt(msg.lower(), self.invertMatrix(key), decrypt=True)
     
-if __name__ == "__main__":
-    #key = [[6, 3], [7,8]]
-    key = [[3,2], [8,5]]
-    msg = "MUBYA QIQGN AEWOS RZQJI RZQKC LIZAG SXCJA AQFRM HO"
-    """key = ([[1,2,3],
-            [0,1,4],
-            [5,6,0]])"""
-    cryptoSystem = HillSystem(key)
-    #print cryptoSystem.det(key)
-    #print cryptoSystem.getAdjugateMatrix(key)
-    #print cryptoSystem.invertMatrix(key)
-    #print key
-    #print cryptoSystem.encrypt("testing")
-    print cryptoSystem.decrypt(msg)
+if __name__ == "__main__":    
+    parser = argparse.ArgumentParser(description='Encrypt or decrypt a message!')
+    parser.add_argument('-d', '--decrypt', dest='decrypt', action='store_true',
+                       default=False, help='Whether to decrypt the message. \
+                       (Default action is to encrypt the message.)')
+    parser.add_argument('--msg', dest='msg', action='store',
+                       help='The message to encrypt or decrypt.', 
+                       required=True)
+    parser.add_argument('--key', dest='key', action='store', nargs='+', type=int,
+                       help='The key used to encrypt or decrypt.', 
+                       required=True)
 
+    args = parser.parse_args()
+    
+    # Initialize the crypto system
+    cryptoSystem = HillSystem()    
+    
+    #key = [[3,2], [8,5]]
+    # the 'dimension' of the array is the square root of the length since it
+    # is a square matrix
+    arrDim = int(math.sqrt(len(args.key)))
+    # Initialize the key (The argument is a 1D array it needs to be converted
+    # into a square matrix)
+    key = [args.key[i:i+arrDim] for i in range(0, len(args.key), arrDim)]
+    
+    # Initialize the message to encrypt or decrypt
+    #msg = "MUBYA QIQGN AEWOS RZQJI RZQKC LIZAG SXCJA AQFRM HO"
+    msg = args.msg
+    
+    if not args.decrypt:
+        print "Encrypting message:\n%s\n" % msg
+        print "Using key:\n%s\n" % key
+        ciphertext = cryptoSystem.encrypt(msg, key)
+        print "Ciphertext:\n%s\n" % ciphertext
+    else:
+        print "Decrypting message:\n%s\n" % msg
+        print "Using key:\n%s\n" % key
+        plaintext = cryptoSystem.decrypt(msg, key)
+        print "Plaintext:\n%s\n" % plaintext
