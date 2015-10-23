3a3b460fdfd9fd4700acd1ebb3b5168fafbd646c
Caleb Piekstra <piekstra17@up.edu>
2015-10-23 15:42:07 -0700

    Monoalphabetic cipher implemented
    The idea behind this is to perform cryptanalysis on ciphertext and then
    try to predict what letters in the plaintext correspond to certain
    letters in the ciphertext.
    
    This is done using frequency analysis of the ciphertext and comparing it
    with the English language. This works because in a monoalphabetic
    cipher, letter frequencies are maintained.

--- common/frequency_analysis.py
+++ common/frequency_analysis.py
@@ -1,41 +1,58 @@
 import string
+import operator
 
 class LetterFrequency:
     def __init__(self):
         # initialize the standard english frequency dict
-         self.stdDict =  = {'e': 12.702, 't': 9.056, 'a': 8.167, 'o': 7.507, 'i': 6.966, 'n': 6.749, 's': 6.327, 'h': 6.094, 'r': 5.987}
-         self.stdTuples = sorted(self.stdDict.items(), key=operator.itemgetter(1)).reverse()
+         self.stdDict = {'e': 12.702, 't': 9.056, 'a': 8.167, 'o': 7.507, 'i': 6.966, 'n': 6.749, 's': 6.327, 'h': 6.094, 'r': 5.987}
+         self.stdTuples = sorted(self.stdDict.items(), key=operator.itemgetter(1), reverse=True)
             
     
     ## Determines frequencies of letters in the msg
     def getFrequencies(self, msg):
-        msgLen = len(self.msg)
+        msgLen = len(msg)
         
         frequencies = {}
         for ch in string.ascii_uppercase:
-            frequencies[ch] = (self.msg.count(ch)/float(msgLen))*100
+            frequencies[ch] = (msg.count(ch)/float(msgLen))*100
         
         # return a tuple of the frequencies dict and a list of tuples
         # of (let, letFreq) sorted from greatest frequency to least
-        return (frequencies, sorted(self.freqDict.items(), key=operator.itemgetter(1)).reverse())
+        return (frequencies, sorted(frequencies.items(), key=operator.itemgetter(1), reverse=True))
     
     
     ## getStandardFrequencies
     #
     # returns a tuple of a dictionary and a list of tuples
     # 
-    # The dictionary and tuples contain most common letters found in 
+    # The dictionary and list of tuples contain most common letters found in 
     # English sentences with their corresponding frequencies
     # 
-    # The TODO
+    # The list of tuples is sorted from greatest to least    
+    #
     def getStandardFrequencies(self):
         return (self.stdDict, self.stdTuples)
 
 class DigraphFrequency:
+
+    def __init__(self):
+        # initialize the standard english frequency dict
+        self.stdDict = {
+            'TH': 1.52, 
+            'HE': 1.28, 
+            'IN': 0.94, 
+            'ER': 0.94,
+            'AN': 0.82, 
+            'RE': 0.68, 
+            'ND': 0.63, 
+            'AT': 0.59,
+            'ON': 0.57 
+        }
+        self.stdTuples = sorted(self.stdDict.items(), key=operator.itemgetter(1), reverse=True)
     
-    def getFrequencies(self):
+    def getFrequencies(self, msg):
         msgLen = len(msg)/2
-        
+
         # create a dict of digraphs in the message and their counts
         digraphDict = {}
         for i in range(0, len(msg), 2):
@@ -46,28 +63,31 @@ class DigraphFrequency:
             # otherwise increment the count for that digraph
             else:
                 digraphDict[digraph] += 1
-            
-        
+                    
         # determine the number of unique digraphs
         numDigraphs = len(msg)/2.0
-        
+
         # update the dict to contain frequencies instead of counts
         for digraph, count in digraphDict.iteritems():
             digraphDict[digraph] = round((count / numDigraphs) * 100, 2)
+
+        # return a tuple of the frequencies dict and a list of tuples
+        # of (digraph, digraphFreq) sorted from greatest frequency to least
+        return (digraphDict, sorted(digraphDict.items(), key=operator.itemgetter(1), reverse=True))
         
-        # get a list of tuples containing the digraphs and their corresponding frequencies
-        frequencies = digraphDict.items()
-        
-        # returns the list of tuples sorted from greatest frequency to least
-        return sorted(frequencies, key=lambda x: -x[1])    
     
-    ## getDigraphFrequencies
+    ## getStandardFrequencies
     #
-    # returns the most common digraphs found in English sentences
-    # as well as their corresponding frequencies
+    # returns a tuple of a dictionary and a list of tuples
+    # 
+    # The dictionary and list of tuples contain most common digraphs found in 
+    # English sentences with their corresponding frequencies
+    # 
+    # The list of tuples is sorted from greatest to least
     #
-    def getDigraphFrequencies(self):
-        return [('TH', 1.52), ('HE', 1.28), ('IN', 0.94), ('ER', 0.94), ('AN', 0.82), ('RE', 0.68), ('ND', 0.63), ('AT', 0.59), ('ON', 0.57)]
+    def getStandardFrequencies(self):
+        return (self.stdDict, self.stdTuples)
+        
  
 if __name__ == "__main__":    
     # manual run
--- monoalphabetic_cipher.py
+++ monoalphabetic_cipher.py
@@ -1,121 +1,160 @@
 import argparse
 from common.frequency_analysis import LetterFrequency, DigraphFrequency
 
+class MonoalphabeticCipher:
+
+    ## __init__
+    #
+    # The constructor
+    #
+    def __init__(self):
+        self.multInv = {
+            1:1,
+            3:9,
+            5:21,
+            7:15,
+            9:3,
+            11:19,
+            15:7,
+            17:23,
+            19:11,
+            21:5,
+            23:17,
+            25:25
+        }        
+    
+    def letToInt(self, let):
+        return ord(let.lower()) - 96
+
+    ## getAffineKey
+    #
+    # Using the algorithm from "Cryptological Mathematics" by 
+    # Robert Edward Lewand on page 38
+    #
+    # Suppose it is known for an affine cipher with additive key r 
+    # and multiplicative key s, that plaintext position p1 maps into
+    # ciphertext position c1 and plaintext position p2 maps into 
+    # ciphertext position c2. 
+    # 
+    # Then: s(r + p1) = c1(mod 26) and s(r + p2) = c2(mod 26)
+    # => s(r + p1 - r1 - p2) = c1 - c2(mod 26)
+    # => s(p1 - p2) = c1 - c2(mod 26)
+    #
+    def getAffineKey(self, p1, p2, c1, c2):
+        lhs = (p2 - p1)
+        rhs = (c2 - c1) % 26
+        
+        s = -1
+        for multKey in self.multInv.keys():
+            if lhs * multKey % 26 == rhs:
+                s = multKey
+                break
+        if s == -1:
+            print "ERROR: could not find multiplicative key\n"
+            return None
+            
+        rhs = c1 - s*p1%26
+        if rhs < 0:
+            rhs += 26
+
+        r = -1
+        for addKey in range (0, 26):
+            if addKey * s % 26 == rhs:
+                r = addKey
+                break
+        if r == -1:
+            print "ERROR: could not find additive key\n"
+            return None
+
+        # key is s, r
+        key = (s, r)
+        print "Key:", key, "\n"
+        return key
+        
+    ## decrypt
+    #
+    # @param self - The object pointer
+    # @param msg - The message to decrypt
+    # @param mfc - The two most frequent ciphertext letters
+    # @param mfp - The two most frequent plaintext letters
+    #
+    def decrypt(self, msg, mfc, mfp, verbose=False, showDigraphFrequencies=False):
+        # remove spaces from the message
+        msg = ''.join(msg.split())        
+        if verbose: print "Attempting to decipher:\n", msg, "\n"
+        
+        # get the message and default letter frequencies
+        letFreq = LetterFrequency()
+        (freqDict, freqTuples) = letFreq.getFrequencies(msg)
+        (stdDict, stdTuples) = letFreq.getStandardFrequencies()
+        
+        # if the mfc is None then use the default top two most frequent
+        # letters in the ciphertext message
+        if mfc is None:
+            mfc = (freqTuples[0][0], freqTuples[1][0])
+        else:
+            mfc = tuple(mfc)
+        mfp = tuple(mfp)
+        
+        if verbose: 
+            print "Ciphertext Letter Frequencies:\n%s\n" % freqTuples
+            print "English Language Letter Frequencies:\n%s\n" % stdTuples 
+            if showDigraphFrequencies:
+                diFreq = DigraphFrequency()
+                diFreqTuples = diFreq.getFrequencies(msg)[1]
+                diStdTuples = diFreq.getStandardFrequencies()[1]
+                print "Ciphertext Digraph Frequencies:\n%s\n" % diFreqTuples
+                print "English Language Digraph Frequencies:\n%s\n" % diStdTuples 
+                
+            print "Using letter in ciphertext: %s" % mfc[0]
+            print "\twith frequency: %.2f%%" % freqDict[mfc[0]]
+            print "Using letter in ciphertext: %s" % mfc[1]
+            print "\twith frequency: %.2f%%\n" % freqDict[mfc[1]]
+
+            print "Assuming that", mfc[0], "is equivalent to", mfp[0]
+            print "Assuming that", mfc[1], "is equivalent to", mfp[1], "\n"
+        
+        # get the affine key using the mfc letters and mfp letters
+        key = self.getAffineKey(*tuple(map(self.letToInt, mfc+mfp)))
+        
+        if key is None:
+            print "Unable to decipher using '%s'->'%s' and '%s'->'%s'\n" % (mfc[0], mfp[0], mfc[1], mfp[1])
+            return None
+        
+        # Use the key to decrypt
+        plaintext = ""
+        for ch in msg.lower():
+            cipherInt = ((ord(ch) - 96)*self.multInv[key[0]] - key[1]) % 26
+            # cover the 'z' case
+            if cipherInt == 0:
+                cipherInt = 26
+            plaintext += chr(cipherInt + 96)
+        
+        return plaintext
+        
 if __name__ == "__main__":    
     parser = argparse.ArgumentParser(description='Decrypt a message that was encrypted using a monoalphabetic cipher.')
     parser.add_argument('--msg', dest='msg', action='store',
                        help='The message to decrypt.', 
                        required=True)
     parser.add_argument('--mfc', dest='mfc', action='store', nargs=2, type=str,
-                       help='The two most frequent ciphertext letters.', required=False)
+                       help='The two most frequent ciphertext letters.', required=False, default=None)
     parser.add_argument('--mfp', dest='mfp', action='store', nargs=2, type=str,
                        help='The two (most frequent) plaintext letters that the ciphertext letters are expected to be decrypted to based on frequency analysis.', 
                        required=False, default=['e', 't'])
-
+    parser.add_argument('--sdf', dest='sdf', action='store_true', 
+                       help="Whether to show the digraph frequencies of the ciphertext and the English Language.", 
+                       required=False, default=False)
+                       
     args = parser.parse_args()
     
     #ciphertext = "LTG ZRH JGJ WYE DRK XUC SLK SCG UGZ KWI LXF CSA QUL JRA SWD HZZ HBG NHU MAH RUY PIY LES SSG SLJ RAG DOH NWZ CXK WGZ MIT LJR ABW JSZ SEZ KKD BJO KOZ GQS GJW VOK WVG L"
     
-    print "Attempting to decipher:\n", ciphertext, "\n"
-
-    # get the letter frequencies
-    letFreq = LetterFrequency()
-    (freqDict, freqTuples) = letFreq.getFrequencies(ciphertext)
-    print "Letter Frequencies:\n", freqTuples, "\n"
-
-    # If most frequent letters in ciphertext were override by manual
-    # user arguments then use those user-specified letters
-    if args.mfc is not None:
-        # most frequent letter in ciphertext
-        cMostFreq1 = args.mfc[0]
-        cMostFreq2 = args.mfc[1]
-    # set most and second most frequent letter in ciphertext
+    monCi = MonoalphabeticCipher()
+    plaintext = monCi.decrypt(args.msg, args.mfc, args.mfp, verbose=True, showDigraphFrequencies=args.sdf)
+    if plaintext:
+        print "Plaintext:\n", plaintext
     else:
-        cMostFreq1 = frequencies[0][0]        
-        cMostFreq2 = frequencies[1][0]
-
-    print "Most frequent letter in ciphertext:", cMostFreq1
-    print "\twith frequency: %.2f%%" % frequencies[0][1]
-    print "Second most frequent letter in ciphertext:", cipherLetter2
-    print "\twith frequency: %.2f%%\n" % frequencies[1][1]
-
-    # assume that the two most frequent letters are 'e' and 't' respectively
-    # unless manual guesses are specified via command line
-    if len(sys.argv) <= 1:
-        mostFreq = 'e'
-        secondMostFreq = 't'
-    else:
-        mostFreq = sys.argv[1]
-        secondMostFreq = sys.argv[2]
-
-    print "Assuming that", cipherLetter1, "is equivalent to", mostFreq
-    print "Assuming that", cipherLetter2, "is equivalent to", secondMostFreq, "\n"
-
-    # quick lookup table of multiplicative inverses for integers mod 26
-    multiplicativeInverse = {
-        1:1,
-        3:9,
-        5:21,
-        7:15,
-        9:3,
-        11:19,
-        15:7,
-        17:23,
-        19:11,
-        21:5,
-        23:17,
-        25:25
-    }
-
-    # initialize the variables to the mod 26 equivalents of the letters
-    # plaintext characters
-    p1 = ord(mostFreq) - 96
-    p2 = ord(secondMostFreq) - 96
-    # ciphertext characters
-    c1 = ord(cipherLetter1.lower()) - 96
-    c2 = ord(cipherLetter2.lower()) - 96
-
-    lhs = (p2 - p1)
-    rhs = (c2 - c1) % 26
-
-    s = -1
-    for multKey in multiplicativeInverse.keys():
-        if lhs * multKey % 26 == rhs:
-            s = multKey
-            break
-    if s == -1:
-        print "ERROR: could not find multiplicative key"
-        sys.exit(-1)
-        
-    rhs = c1 - s*p1%26
-    if rhs < 0:
-        rhs += 26
-
-    r = -1
-    for addKey in range (0, 26):
-        if addKey * s % 26 == rhs:
-            r = addKey
-            break
-    if r == -1:
-        print "ERROR: could not find additive key"
-        sys.exit(-1)
-
-    # key is s, r
-    key = (s, r)
-    print "Key:", key, "\n"
-
-    plaintext = ""
-    for ch in ciphertext.lower():
-        if ch == ' ':
-            continue
-        else:
-            cipherInt = ((ord(ch) - 96)*multiplicativeInverse[key[0]] - key[1]) % 26
-            # cover the 'z' case
-            if cipherInt == 0:
-                cipherInt = 26
-            plaintext += chr(cipherInt + 96)
-
-    print "Plaintext:\n", plaintext
-
+        print "Could not decrypt the message"
 
 
